0a637655b61a847ce2b1d08305dddbdd4f7da239
alonhi <karsonqqq@gmail.com>
2024-05-19 01:43:54 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Аниме Сайт</title>
+    <style>
+        /* Стили для лого и кружка */
+        .header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 10px;
+            background-color: #f1f1f1;
+        }
+        
+        .logo img {
+            height: 50px;
+        }
+        
+        .account-circle {
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+            background-color: black;
+        }
+    </style>
+</head>
+<body>
+    <div class="header">
+        <div class="logo">
+            <img src="logo.png" alt="Лого">
+        </div>
+        <div class="account-circle"></div>
+    </div>
+    
+    <div class="content">
+        <!-- Здесь будет содержимое главной страницы -->
+    </div>
+    
+</body>
+</html>
